cc13939020b19fbcff79ac235ee2a37ef81d0f3e
ms-jahan <ms-jahan@users.noreply.github.com>
2026-06-15 15:00:00 +0600

    start project, drop old tables

---
+++ QUERY.sql
@@ -0,0 +1,7 @@
+-- Football Ticket Booking System
+-- Part 1: DDL + sample data | Part 2: Queries
+
+-- drop in reverse FK order to avoid constraint errors
+DROP TABLE IF EXISTS Bookings;
+DROP TABLE IF EXISTS Matches;
+DROP TABLE IF EXISTS Users;
